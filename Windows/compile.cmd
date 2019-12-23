28a4ad49e30f678a45c4c0bad3a57b5ad318fb92
Berry de Jager <berry@berrydejager.com>
2019-12-23 16:50:50 +0100

    Added rastertiming
    Added rastertiming

--- Windows/compile.cmd
+++ Windows/compile.cmd
@@ -3,6 +3,6 @@ cls
 java -jar .\Prerequisites\KickAssembler\KickAss.jar ..\SourceBin\main.src -showmem 
 if %errorlevel%==0 .\Prerequisites\WinVICE\x64mm.exe +VICIIdsize -VICIIborders 2 +confirmexit ..\SourceBin\main.prg
 if not %errorlevel%==0 pause
-./compile.cmd
+.\compile.cmd
 pause
 
